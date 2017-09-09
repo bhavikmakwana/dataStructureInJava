6604b19782fc78ba6c9fc67682ebc9d092cccce8
Bhavik <bhavikmakwana1993@gmail.com>
2017-09-09 16:56:20 +0530

    Stack

--- Stack/src/Stack.java
+++ Stack/src/Stack.java
@@ -1,2 +1,48 @@
 public class Stack {
+    int[] stk;
+    int top;
+
+    public Stack(int size) {
+        stk = new int[size];
+        top = stk.length ;
+    }
+
+    public void push(int value) {
+        if (top == 0) {
+            System.err.println("Stack Overflow You can not push value ");
+        } else {
+            top--;
+            stk[top] = value;
+        }
+    }
+
+    public int pop() {
+        if (top == stk.length) {
+            System.err.println("Stack Underflow Stack is Empty you can not pop value");
+            return 0;
+        }else{
+            int temp = stk[top];
+            top++;
+            return temp;
+        }
+
+    }
+
+    public void display() {
+        System.out.println("-----------Printing Stack-----------");
+        for (int i = top; i < stk.length; i++) {
+            System.out.println(stk[i]);
+        }
+    }
+
+    public void search(int value) {
+        for (int i = top; i < stk.length; i++) {
+            if (value == stk[i]){
+                System.out.println("Value found at : " + i);
+            }
+            else{
+                System.err.println("Value not found !!");
+            }
+        }
+    }
 }
--- Stack/src/Test.java
+++ Stack/src/Test.java
@@ -1,6 +1,49 @@
-public class Test {
-    public static void main(String [] args ){
-        System.out.println("Welcome to Stack");
+import java.util.Scanner;
 
+/*
+    @Author : Bhavik Makwana
+    @since : 9/09/2017
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Stack");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the size of Stack");
+        int size = scanner.nextInt();
+        Stack stack = new Stack(size);
+        while (true) {
+            System.out.println("########## Menu ##########");
+            System.out.println("1.Push");
+            System.out.println("2.Pop");
+            System.out.println("3.Search ");
+            System.out.println("4.display");
+            System.out.println("5.Exit");
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter the value for Push ? ");
+                    int pushValue = scanner.nextInt();
+                    stack.push(pushValue);
+                    break;
+                case 2:
+                    stack.pop();
+                    break;
+                case 3:
+                    System.out.println("Enter the value for Search ? ");
+                    int searchValue = scanner.nextInt();
+                    stack.search(searchValue);
+                    break;
+                case 4:
+                    stack.display();
+                    break;
+                case 5:
+                    System.exit(0);
+                    break;
+                default:
+                    System.err.println("invalid choice !!");
+            }
+
+        }
     }
 }
